7513b82574a32485a82a5f1c585647cb3f1b34e0
oliverwillms4040-afk <oliverwillms4040@gmail.com>
2026-08-25 14:56:04 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="promocanvas-bot",
+    version="1.0.0",
+    install_requires=[
+        "python-telegram-bot==20.7",
+        "python-dotenv==1.0.0",
+        "Pillow==10.1.0",
+        "requests==2.31.0",
+        "aiohttp==3.9.0",
+    ],
+)
